0498a7d8c30d9597c04219ce4fd3bf4b8dc10027
Daniel Echeverri <daniel.echeverri@flyr.com>
2026-01-11 13:53:47 -0500

    Correct Mapping profile to ignore Id, CreatedDate and Computed columns.

--- TechnicalTestWTW/Mappings/PersonaMappingProfile.cs
+++ TechnicalTestWTW/Mappings/PersonaMappingProfile.cs
@@ -8,9 +8,11 @@ public class PersonaMappingProfile : Profile
     public PersonaMappingProfile()
     {
         CreateMap<Persona, PersonaDto>();
-        
-        CreateMap<PersonaDto, Persona>()
-            .ForMember(dest => dest.FullName, opt => opt.Ignore())
-            .ForMember(dest => dest.FullIdentificationNumber, opt => opt.Ignore());
-    }
+
+		CreateMap<PersonaDto, Persona>()
+			.ForMember(dest => dest.Id, opt => opt.Ignore())
+			.ForMember(dest => dest.CreatedDate, opt => opt.Ignore())
+			.ForMember(dest => dest.FullName, opt => opt.Ignore())
+			.ForMember(dest => dest.FullIdentificationNumber, opt => opt.Ignore());
+	}
 }
